e55b5da62f0acab025776c1c718c7eb98109fdb1
kibuniverse <3050540005@qq.com>
2020-09-04 09:09:15 +0800

    feat: 增加添加文件组件

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,35 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <UploadBox :addFile="addFile" />
+    
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
+import UploadBox from "./components/Upload.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+    UploadBox,
+  },
+  data () {
+    return {
+      waitCalculated: []
+    }
+  },
+  methods: {
+    addFile(files) {
+      for(let i = 0, len = files.length; i < len; i++) {
+        this.waitCalculated.push(files[i])
+      }
+    }
+  },
+};
 </script>
 
 <style>

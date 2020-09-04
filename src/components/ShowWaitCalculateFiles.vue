80e6b0c86f1b1cfc6b48be50ccd95bdbcad39f83
kibuniverse <3050540005@qq.com>
2020-09-04 09:49:09 +0800

    perfect: 展示待计算文件完成

--- src/components/ShowWaitCalculateFiles.vue
+++ src/components/ShowWaitCalculateFiles.vue
@@ -1,12 +1,41 @@
 <template>
-  
+  <div class='wait-box'>
+      <ul id='item-box'>
+          <li v-for="item in waitCalculateFiles" :key="item.name">
+              {{item.name}}
+          </li>
+      </ul>
+  </div>
 </template>
 
 <script>
 export default {
+    name: 'ShowWaitCalculateFiles',
+    props: {
+        waitCalculateFiles: []
+    },
 }
 </script>
 
 <style>
+.wait-box {
+    width: 400px;
+    margin: 20px auto auto;
+    border-radius: 10px;
+    text-align: center;
+    color: blue;
+    padding: 10px;
+    border: 1px solid rgb(80, 153, 237);
+    background-color: rgb(255, 255, 255);
+    transition: all 1s ease 0s;
+    display: block;
+}
 
+#item-box {
+    list-style: none;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 0px;
+}
 </style>
